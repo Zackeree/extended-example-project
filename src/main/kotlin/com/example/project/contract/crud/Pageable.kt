2baf3170d358bb16a5b8c97f4140ea1c1a056b33
cody <cspath1@ycp.edu>
2018-07-12 16:38:26 -0400

    add gradle build script to verify contract coverage

--- src/main/kotlin/com/example/project/contract/crud/Pageable.kt
+++ src/main/kotlin/com/example/project/contract/crud/Pageable.kt
@@ -1,3 +1,29 @@
 package com.example.project.contract.crud
 
-interface Pageable
+import com.example.project.contract.Command
+import com.example.project.contract.responder.PageResponder
+import com.google.common.collect.Multimap
+import org.springframework.data.domain.Page
+
+interface Pageable<REQUEST, INFO, ERROR_TAG> {
+    fun page(request: REQUEST, responder: PageResponder<INFO, ERROR_TAG>): Command
+
+    fun page(request: REQUEST): SimplePageResult<INFO, ERROR_TAG> {
+        var result = SimplePageResult<INFO, ERROR_TAG>(null, null)
+        page(
+                request = request,
+                responder = object : PageResponder<INFO, ERROR_TAG> {
+                    override fun onSuccess(t: Page<INFO>) {
+                        result = SimplePageResult(t, null)
+                    }
+
+                    override fun onFailure(e: Multimap<ERROR_TAG, String>) {
+                        result = SimplePageResult(null, e)
+                    }
+                }
+        ).execute()
+        return result
+    }
+}
+
+data class SimplePageResult<INFO, ERROR_TAG>(val success: Page<INFO>?, val error: Multimap<ERROR_TAG, String>?)
